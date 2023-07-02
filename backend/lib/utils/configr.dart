dfc69e6a56f37a7d63b1e442c6337be5fe00056a
Rashmi Wijesooriya <it20668686@my.sliit.lk>
2023-07-02 21:53:07 +0530

    backend

---
+++ backend/lib/utils/configr.dart
@@ -0,0 +1,23 @@
+class Configr {
+  //Screen Test welcome Screen
+  static const home_background = 'assets/screenTestAssets/HomeScreen.gif';
+  static const home_icon = 'assets/screenTestAssets/icon-home.png';
+  static const profile_icon = 'assets/screenTestAssets/icon-profile.png';
+  static const back_icon = 'assets/screenTestAssets/icon-back.png';
+
+  //Word list
+  static const dog_list = 'assets/screenTestAssets/DogList.gif';
+  static const goat_list = 'assets/screenTestAssets/GoatList.gif';
+  static const spider_list = 'assets/screenTestAssets/SpiderList.gif';
+
+  //Record audi
+  static const dog_in = 'assets/screenTestAssets/DogIn.gif';
+
+  //texts
+  static const start = "අරඹමු";
+  static const learn = "දැනගමු";
+  static const word_pg = "වචන පිටුව";
+  static const select_any = "කැමති වචනයක් තෝරන්න";
+  static const whos = "මේ කවුද?";
+  static const word_game = "වචන සෙල්ලම";
+}
